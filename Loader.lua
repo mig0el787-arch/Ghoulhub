437f1420e54ff818db16adb7d0a8bd2572adc0f5
mig0el787-arch <mig0el787@gmail.com>
2026-01-12 08:18:34 -0300

    Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,11 +1,7 @@
--- Ghoul Hub Loader (Drawing)
-
 if not game:IsLoaded() then
     game.Loaded:Wait()
 end
 
-pcall(function()
-    loadstring(game:HttpGet(
-        "https://raw.githubusercontent.com/migoel787-arch/Ghoulhub/main/DrawingUI.lua"
-    ))()
-end)
+loadstring(game:HttpGet(
+    "https://raw.githubusercontent.com/migoel787-arch/Ghoulhub/main/DrawingUI.lua"
+))()
