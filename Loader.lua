9a947b7dabc565e82e69a03fa3a782a7a77418dd
mig0el787-arch <mig0el787@gmail.com>
2026-01-12 07:19:17 -0300

    Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,31 @@
+-- Ghoul Hub Loader ☠️
+
+if not game:IsLoaded() then
+	game.Loaded:Wait()
+end
+
+local Players = game:GetService("Players")
+local Player = Players.LocalPlayer
+
+-- Proteção simples
+if Player:WaitForChild("PlayerGui"):FindFirstChild("GhoulHub") then
+	return
+end
+
+-- Pasta do Hub
+local BASE_URL = "https://raw.githubusercontent.com/SEU_USUARIO/GhoulHub/main/"
+
+-- Carrega módulos
+local function load(file)
+	return loadstring(game:HttpGet(BASE_URL .. file))()
+end
+
+-- Música (5 segundos)
+pcall(function()
+	load("Modules/Music.lua").Play()
+end)
+
+-- Interface
+pcall(function()
+	load("Modules/UI.lua")
+end)
