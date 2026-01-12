5df65f499b142f5a73b85db4d104491ca3b3d3bd
mig0el787-arch <mig0el787@gmail.com>
2026-01-12 08:22:44 -0300

    Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,7 +1,27 @@
 if not game:IsLoaded() then
-    game.Loaded:Wait()
+	game.Loaded:Wait()
 end
 
-loadstring(game:HttpGet(
-    "https://raw.githubusercontent.com/migoel787-arch/Ghoulhub/main/DrawingUI.lua"
-))()
+local BASE = "https://raw.githubusercontent.com/migoel787-arch/Ghoulhub/main/"
+
+local function load(file)
+	return loadstring(game:HttpGet(BASE .. file))()
+end
+
+-- Intro (opcional)
+pcall(function()
+	load("Intro.lua")
+end)
+
+-- Música (5s)
+pcall(function()
+	local Music = load("Music.lua")
+	if Music and Music.Play then
+		Music:Play()
+	end
+end)
+
+-- UI (OBRIGATÓRIO)
+pcall(function()
+	load("Ui.lua")
+end)
